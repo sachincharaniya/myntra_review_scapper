5da5beb8182b54ba8734711eb0e533d2e6c62dca
Sachin Charaniya <sachin.charaniya13@gmail.com>
2024-10-24 13:37:54 -0900

    myntra review project

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="scrapper",
+    version ='0.0.1',
+    author='sachin',
+    author_email='sachin.charaniya13@gamil.com',
+    packages=find_packages(),
+    install_requires=[  
+        
+    ],  
+)
